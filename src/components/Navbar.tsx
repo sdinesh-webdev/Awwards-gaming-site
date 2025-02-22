7163b18a923221542673aedf7490dcb5c83e8d0c
Dinesh7899 <sdineshku7899@gmail.com>
2025-02-22 15:17:04 +0530

    navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import {useRef} from 'react'
+import Button from './Button'
 
-function Navbar() {
+const Navbar  = () => {
+    const navContainerRef  = useRef(null);
   return (
-    <div>Navbar</div>
+    <div ref={navContainerRef} className="fixed inset-x-0 top-4 z-50 h-16 border-none transition-all duration-700 sm:inset-x-6">
+    <header className="absolute top-1/2 w-full-translate-y-1/2">
+    <nav className="flex size-full items-center justify- p-4">
+        <div className="flex items-center gap-7">
+            <img src="/img/logo.png" alt="logo" 
+            className='w-10'/>
+
+            <Button 
+            id='product-button'
+            title='Products'
+            rightIcon='{<TiLocationArrow/>}
+            containerClass="bg-blue-50 md:flex hidden items-center justify-center gap-1'
+            />
+        </div>
+    </nav>
+    </header>
+    </div>
   )
 }
 
-export default Navbar
+export default  Navbar;
