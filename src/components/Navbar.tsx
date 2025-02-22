1c27a080bf84c451011ec3a07846a38ec7769456
Dinesh7899 <sdineshku7899@gmail.com>
2025-02-22 15:40:48 +0530

    navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,14 @@
 import {useRef} from 'react'
 import Button from './Button'
+import { TiLocationArrow } from 'react-icons/ti'
 
+const navItems = ['Nexus', 'Vault', 'Prologue','About', 'Contact'];
 const Navbar  = () => {
     const navContainerRef  = useRef(null);
+
+    const audioElementRef = useRef(null);
+    const toggleAudioIndicator = () => {}
+
   return (
     <div ref={navContainerRef} className="fixed inset-x-0 top-4 z-50 h-16 border-none transition-all duration-700 sm:inset-x-6">
     <header className="absolute top-1/2 w-full-translate-y-1/2">
@@ -14,9 +20,17 @@ const Navbar  = () => {
             <Button 
             id='product-button'
             title='Products'
-            rightIcon='{<TiLocationArrow/>}
-            containerClass="bg-blue-50 md:flex hidden items-center justify-center gap-1'
+            rightIcon={<TiLocationArrow/>}
+            containerClass="bg-blue-50 md:flex hidden items-center justify-center gap-1"
             />
+        </div>
+        <div className="flex h-full items-center">
+            <div className="hidden md:block ">
+                {navItems.map((item) => (
+                    <a key={item} href={`#${item.toLowerCase()}`} className='nav-hover-btn'>{item}</a>
+                ))}
+            </div>
+            <button className="ml-10 flex items-center space-x-0.5" onClick={toggleAudioIndicator}></button>
         </div>
     </nav>
     </header>
