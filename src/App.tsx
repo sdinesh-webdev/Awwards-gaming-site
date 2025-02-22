0fc9257d089211f6fb14262b24497db5889845e9
Dinesh7899 <sdineshku7899@gmail.com>
2025-02-22 15:00:17 +0530

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Hero from "./components/Hero"
 import About from './components/About'
+import Navbar from './components/Navbar'
 function App() {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Hero />
       <About />
+      <Navbar />
     </main>
   )
 }
